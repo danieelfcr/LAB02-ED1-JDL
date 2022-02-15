9a543820db5488049aa7235cb7fdbd98fc21a008
Jose Mario Marroquin <jmmr114@gmail.com>
2022-02-15 17:34:39 -0600

    PlayerList Insertion method added

--- ClassLibrary/PlayerList.cs
+++ ClassLibrary/PlayerList.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace ClassLibrary
 {
-    class PlayerList
+    class PlayerList<T>
     {
+        Player<T> First;
+        Player<T> Last;
+
+        public bool Insertion(Player<T> newPlayer)
+        {
+            if (First == null)
+            {
+                First = newPlayer;
+                Last = First;
+                return true;
+            }
+            else
+            {
+                Player<T> aux = First;
+
+                while(aux != Last)
+                {
+                    if (aux == null)
+                    {
+                        aux = newPlayer;
+                        Last = aux;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
