c49df2c675e918765663976365fb81edc30d7673
Jose Mario Marroquin <jmmr114@gmail.com>
2022-02-16 21:45:51 -0600

    team CSV file input added

---
+++ LAB02-ED1/Models/TeamModel.cs
@@ -0,0 +1,31 @@
+﻿using LAB02_ED1.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB02_ED1.Models
+{
+    public class TeamModel
+    {
+        [Required]
+        [MaxLength(15)]
+        public string TeamName { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Coach { get; set; }
+
+        [Required]
+        public string League { get; set; }
+        [Required]
+        public DateTime DateCreation { get; set; }
+        
+
+
+        public static void Save(TeamModel model)
+        {
+            DataTeam.Instance.Teamlist.Add(model);
+        }
+    }
+}
